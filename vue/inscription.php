40f0fd20ec20eed26116baffc635cdd30270c07d
AdamWizard <48995051+AdamWizard@users.noreply.github.com>
2020-12-16 17:24:16 +0100

    Update inscription.php

--- vue/inscription.php
+++ vue/inscription.php
@@ -3,13 +3,12 @@
 
 <!DOCTYPE html>
 
-
 <html lang="fr" xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
     <meta charset="utf-8" />
     <title>Inscription</title>
 </head>
-
 
 <body bgcolor="gray">
 
