5848914ce6b436367f4602986697b78af0216ffc
AdamWizard <48995051+AdamWizard@users.noreply.github.com>
2021-01-22 16:25:20 +0100

    fonctions nouvel_appart et nouvel_appareil
    il va falloir récupérer les infos à insérer dans les tables

--- modele/nouvelAppart.php
+++ modele/nouvelAppart.php
@@ -2,6 +2,6 @@
 
 include("modele/connexionBDD.php");
 
-function nouvel_appart($numAppart, $numAppart , $id_deg_cit , $id_type_appart , $id_secu , $id_maison){
+function nouvel_appart(NULL, $numAppart , $id_deg_cit , $id_type_appart , $id_secu , $id_maison){
     mysqli_query($_SESSION['bdd'],"INSERT INTO Appartement VALUES (NULL , $numAppart , $id_deg_cit , $id_type_appart , $id_secu , $id_maison);
 }
